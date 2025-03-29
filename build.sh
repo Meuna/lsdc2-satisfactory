e3ce645694ec39e3238f91403be3def94492787b
Thomas Bany <mun.bany@gmail.com>
2025-03-29 10:23:31 +0100

    Update serverwrap

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-podman build . -t docker.io/meuna/lsdc2:satisfactory --format docker
-podman push docker.io/meuna/lsdc2:satisfactory
+podman build . -t docker.io/meuna/lsdc2:satisfactory --format docker \
+&& podman push docker.io/meuna/lsdc2:satisfactory
